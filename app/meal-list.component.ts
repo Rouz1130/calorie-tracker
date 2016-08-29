e3e1cae7255c1a4eaf15d3ea6e6f3a90e6832d64
rouz majlessi <rmajlessi@gmail.com>
2016-08-28 19:52:57 -0700

    add component and export class to meal list file

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,2 +1,21 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
+
+
+@Component({
+  selector: 'meal-list',
+  inputs: ['mealList'],
+  template: `
+  <div class="container">
+  <h3 *ngFor="#currentMeal of mealList" (click)= "mealClicked(currentMeal)">{{currentMeal.name}}</h3>
+  </div>
+  `
+})
+
+
+export class MealListComponent {
+  public mealList: Meal[];
+  mealClicked(clickedMeal: Meal): void{
+    console.log(clickedMeal);
+  }
+}
