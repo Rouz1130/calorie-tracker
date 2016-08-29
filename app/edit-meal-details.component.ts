d67b6bb9dad5375bc340952b8f71974de6556f00
rouz majlessi <rmajlessi@gmail.com>
2016-08-28 22:13:21 -0700

    add the 3 tasks for edit meal file

--- app/edit-meal-details.component.ts
+++ app/edit-meal-details.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter } from 'angular2/core';
-import {Meal} from './meal.model';
-
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'edit-meal-details',
   inputs: ['meal'],
   template: `
   <div class="meal-form">
-      <h3>Edit Name: </h3>
-      <input [(ngModel)]="meal.name" class="col-sm-8 input-lg meal-form"/>
-    </div>
+  <h4>Change Meal</h4>
+  <input [(ngModel)]="meal.name"/>
+  <h4>Change description</h4>
+  <input [(ngModel)]="meal.details"/>
+  <h4>Adjust Calories Count</h4>
+  <input [(ngModel)]="meal.calories"/>
+  </div>
   `
 })
 export class EditMealDetailsComponent {
