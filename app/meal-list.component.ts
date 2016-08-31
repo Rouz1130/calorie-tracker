69aa0021f5e2822e8dd33404424a0ebecb18a3ab
rouz majlessi <rmajlessi@gmail.com>
2016-08-30 21:08:29 -0700

    add to components directives calories high and low

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -10,8 +10,8 @@ import { CaloriePipe } from './calorie.pipe';
   inputs: ['mealList','onSubmitNewMeal'],
   outputs: ['onMealSelect'],
   directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
+  pipes: [CaloriePipe],
   template: `
-
   <meal-display *ngFor="#currentMeal of mealList" (click)= "mealClicked(currentMeal)" [class.selected]="currentMeal === selectedMeal"[meal]="currentMeal"></meal-display>
   <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal"></edit-meal-details>
   <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
@@ -22,15 +22,21 @@ import { CaloriePipe } from './calorie.pipe';
 export class MealListComponent {
   public mealList: Meal[];
   public onMealSelect: EventEmitter<Meal>;
+  public selectedCompleteness: string = "all";
   public selectedMeal: Meal;
   constructor(){
     this.onMealSelect = new EventEmitter();
   }
   mealClicked(clickedMeal: Meal): void{
-    console.log("child",clickedMeal);
+    console.log("kiddo",clickedMeal);
     this.selectedMeal = clickedMeal;
     this.onMealSelect.emit(clickedMeal);
   }
+  onChange(optionFromMenu){
+    this.selectedCompleteness = optionFromMenu;
+    console.log(this.selectedCompleteness);
+  }
+
   createMeal(meal): void {
     this.mealList.push(
       new Meal(meal.userName, meal.userDetails, meal.userCalories)
